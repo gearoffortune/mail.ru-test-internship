af986561f43ed000c03a4c8a3eb9ca37ddcbf875
gearoffortune <hopelessuntildeath@gmail.com>
2019-07-22 23:13:45 +0300

    changed structure and added readme

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@
 const path = require('path');
 
 module.exports = {
-  entry: './main.js',
+  entry: './component/main.js',
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist')
